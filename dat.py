7aaae6b8b8cbb5d1599e7b2a7821cfbf85345c7b
rvp39 <vipulk3009@gmail.com>
2020-03-15 16:29:20 +0530

    added automatic date feature

--- dat.py
+++ dat.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import pandas as pd
+import datetime
 
 def submit_feild():
     path='C:\\Users\\rvp39\\Desktop\\expenses.xlsx'
@@ -8,7 +9,7 @@ def submit_feild():
     SeriesA=df1['Date']
     SeriesB=df1['Ammount']
     SeriesC=df1['Reason']
-    A=pd.Series(entry1.get())
+    A=pd.Series(entry1)
     B=pd.Series(entry2.get())
     C=pd.Series(entry3.get())
     SeriesA=SeriesA.append(A)
@@ -16,7 +17,7 @@ def submit_feild():
     SeriesC=SeriesC.append(C)
     df2=pd.DataFrame({"Date":SeriesA,"Ammount":SeriesB,"Reason":SeriesC})
     df2.to_excel(path,index = False)
-    entry1.delete(0, END)
+    #entry1.delete(0, END)
     entry2.delete(0, END)
     entry3.delete(0, END)
 
@@ -25,10 +26,12 @@ master=Tk()
 Label(master,text="Date").grid(row=3)
 Label(master,text="Ammount").grid(row=4)
 Label(master,text="Reason").grid(row=5)
-entry1=Entry(master)
+date=datetime.datetime.today()
+entry1=date.strftime("%d/%m/%y")
+Label(master,text=entry1).grid(row=3, column=2)
 entry2=Entry(master)
 entry3=Entry(master)
-entry1.grid(row=3,column=2)
+#entry1.grid(row=3,column=2)
 entry2.grid(row=4,column=2)
 entry3.grid(row=5,column=2)
 Button(master, text="quit",command=master.quit).grid(row=6,column=2,pady=4)
